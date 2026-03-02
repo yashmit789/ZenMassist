1d61385c211d9bc47cb818d9831d9449aa7c7379
Yashmit Gupta <yashmitgupta576@gmail.com>
2026-03-02 18:30:35 +0530

    fix: updated supabase url to bypass ISP block

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // Use the Project URL and Anon Key from your Supabase Dashboard Settings
 export const supabase = createClient(
-  'https://yjxexgxbpyktimmkhqts.supabase.co', 
+  'https://zenflow.jiobase.com', 
   'sb_publishable_fx1elpMg-2EB2C5Zgk2GXA_RjSnsWbf'
 )
